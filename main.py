9ee83fb0d18dc801b430ca9cca42834f23d76175
khominiatych <hominyatichalbina@gmail.com>
2023-12-12 17:01:55 +0200

    дослідження модуля

---
+++ main.py
@@ -0,0 +1,18 @@
+#інтроспекція
+import random
+help(random)
+#pip install requests
+def first_function():
+    pass
+class Human:
+    pass
+rq = random
+first_f = first_function
+nick = Human
+print(random.__name__)
+print(rq.__name__)
+print(nick.__name__)
+print(__name__)
+
+info_lst = []#як дізнатися як працювати зі списками?
+
